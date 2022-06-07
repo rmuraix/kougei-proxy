4535ba093ffdccf5f4a93cccc797c55174ca8868
rmuraix <35632215+rmuraix@users.noreply.github.com>
2022-06-07 20:06:07 +0900

    add git setting

--- kougei-config.py
+++ kougei-config.py
@@ -2,23 +2,26 @@ import subprocess
 from subprocess import PIPE
 
 def main():
-    print("kougei-config 1.0")
-    print("Copyright (C) Ryota Murai")
-    print("repo: https://github.com/rmuraix/kougei-proxy\n")
+    print('kougei-config 1.0')
+    print('Copyright (C) Ryota Murai')
+    print('repo: https://github.com/rmuraix/kougei-proxy\n')
 
-    proc = subprocess.run("netsh wlan show interfaces", shell=True, stdout=PIPE, stderr=PIPE, text=True)
+    proc = subprocess.run('netsh wlan show interfaces', shell=True, stdout=PIPE, stderr=PIPE, text=True)
     datalist = proc.stdout.split('\n')
     for data in datalist :
         if data.find('    SSID') == 0:
             ssid = data[data.find(':')+2:len(data)]
-            print('ssid = '+ssid)
-        elif data.find('    BSSID') == 0:
-            bssid = data[data.find(':')+2:len(data)]
-            print('bssid = '+bssid)
+
+    proxy = 'proxy-a.t-kougei.ac.jp:8080'
+
+    if ssid == 'Kougei-WiFi.1xST':
+        subprocess.run('git config --global http.proxy {}'.format(proxy), shell=True, stdout=PIPE, stderr=PIPE)
+    else:
+        subprocess.run('git config --global --unset http.proxy', shell=True, stdout=PIPE, stderr=PIPE)
 
     subprocess.call('PAUSE', shell=True)
 
     return
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
