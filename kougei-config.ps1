7968cc0c2cd8fd954d200ed604cb958bab842d84
rmuraix <35632215+rmuraix@users.noreply.github.com>
2022-06-09 13:41:37 +0900

    fix #6

--- kougei-config.ps1
+++ kougei-config.ps1
@@ -7,10 +7,8 @@ $SSID=9
 $result=netsh wlan show interfaces
 
 if ( $result[$STATE].split(":")[1].trim() -eq "connected"){
-    Write-Output ($result[$SSID].split(":")[1].trim())
     $SSIDNAME=$result[$SSID].split(":")[1].trim()
 }else{
-    Write-Output "LAN接続なし"
     $SSIDNAME=$null
 }
 
@@ -18,11 +16,9 @@ chcp $code | out-null
 
 if ($SSIDNAME -eq "kougei-WiFi.1xST") {
     git config --global http.proxy proxy-a.t-kougei.ac.jp:8080
-    Write-Output "Gitプロキシ:有効"
 }else{
     git config --global --unset http.proxy
-    Write-Output "Gitプロキシ:無効"
 }
 
-write-host "続行するにはキーを押してください..."
+write-host "Press any key to continue..."
 [void][System.Console]::ReadKey($true)
