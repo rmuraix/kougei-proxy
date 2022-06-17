176eb5b51f185e50904a0331a2f8eaf62854030d
rmuraix <35632215+rmuraix@users.noreply.github.com>
2022-06-17 17:22:35 +0900

    ps1: add pip setting

--- kougei-config.ps1
+++ kougei-config.ps1
@@ -11,11 +11,20 @@ if ( $result[$STATE].split(":")[1].trim() -eq "connected"){
 }else{
     $SSIDNAME=$null
 }
+pip | Out-File -FilePath ./configtemp.txt -Encoding utf8
 
 chcp $code | out-null
 
+$pipfind=Select-String -Path ./configtemp.txt -Pattern Usage
+
+Remove-Item -Recurse ./configtemp.txt
+
 if ($SSIDNAME -eq "kougei-WiFi.1xST") {
     git config --global http.proxy proxy-a.t-kougei.ac.jp:8080
+    if ($pipfind -like "*configtemp.txt*") {
+        pip config set global.proxy proxy-a.t-kougei.ac.jp:8080
+    }
 }else{
     git config --global --unset http.proxy
+    pip config unset global.proxy
 }
